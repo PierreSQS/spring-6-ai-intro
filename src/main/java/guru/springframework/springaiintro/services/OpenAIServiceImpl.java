ab97f8a36de9e026d2707e884fc03a33530ef725
PierreSQS <nomo_messina@web.de>
2025-02-13 18:59:35 +0100

    Sec3_Chap20-XXX: upgraded code to SB 3.4.x

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -2,39 +2,42 @@ package guru.springframework.springaiintro.services;
 
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.Question;
-import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot on 12.02.2025.
  */
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    private final ChatModel chatModel;
+    private final ChatClient chatClient;
 
-    public OpenAIServiceImpl(ChatModel chatModel) {
-        this.chatModel = chatModel;
+    public OpenAIServiceImpl(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
     }
 
     @Override
     public Answer getAnswer(Question question) {
         PromptTemplate promptTemplate = new PromptTemplate(question.question());
         Prompt prompt = promptTemplate.create();
-        ChatResponse response = chatModel.call(prompt);
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
 
-        return new Answer(response.getResult().getOutput().getContent());
+        return new Answer(Objects.requireNonNull(response).getResult().getOutput().getContent());
     }
 
     @Override
     public String getAnswer(String question) {
         PromptTemplate promptTemplate = new PromptTemplate(question);
         Prompt prompt = promptTemplate.create();
-        ChatResponse response = chatModel.call(prompt);
+        ChatResponse response = chatClient.prompt(prompt).call().chatResponse();
 
+        assert response != null;
         return response.getResult().getOutput().getContent();
     }
 }
