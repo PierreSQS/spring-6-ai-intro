038f4fe0a72c21994157902995f3628b1ff430b8
PierreSQS <nomo_messina@web.de>
2025-02-22 12:13:27 +0100

    Sec42_Chap397-XXX: optimized test code

--- src/test/java/guru/springframework/springaiintro/SpringAiIntroApplicationTests.java
+++ src/test/java/guru/springframework/springaiintro/SpringAiIntroApplicationTests.java
@@ -2,12 +2,17 @@ package guru.springframework.springaiintro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringAiIntroApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        System.out.println("Loaded context");
+        assertThat(appCtx).isNotNull();
     }
 
 }
