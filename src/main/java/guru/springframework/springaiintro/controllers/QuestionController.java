f5b150a559d4520a7aaa4cd7d0f03ec5ca1b06f4
John Thompson <john@springframework.guru>
2025-06-23 09:03:19 -0400

    update to GA

--- src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
@@ -19,11 +19,6 @@ public class QuestionController {
         this.openAIService = openAIService;
     }
 
-    @PostMapping("/capitalWithInfo")
-    public Answer getCapitalWithInfo(@RequestBody GetCapitalRequest getCapitalRequest) {
-        return this.openAIService.getCapitalWithInfo(getCapitalRequest);
-    }
-
     @PostMapping("/capital")
     public Answer getCapital(@RequestBody GetCapitalRequest getCapitalRequest) {
         return this.openAIService.getCapital(getCapitalRequest);
