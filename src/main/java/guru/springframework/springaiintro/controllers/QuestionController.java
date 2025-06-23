b426bd41c6652ec52b8ab5ee729ff1f1367b443a
John Thompson <john@springframework.guru>
2025-06-23 06:32:24 -0400

    update to GA

--- src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class QuestionController {
-
     private final OpenAIService openAIService;
 
     public QuestionController(OpenAIService openAIService) {
