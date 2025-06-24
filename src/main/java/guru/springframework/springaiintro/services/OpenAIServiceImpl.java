3cd82f7bc52a074a0cd369424f999599d7250d46
John Thompson <john@springframework.guru>
2025-06-24 08:51:41 -0400

    update to GA

--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -1,15 +1,15 @@
 package guru.springframework.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
-import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -48,18 +48,25 @@ public class OpenAIServiceImpl implements OpenAIService {
     }
 
     @Override
-    public GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest) {
-        BeanOutputConverter<GetCapitalResponse> converter = new BeanOutputConverter<>(GetCapitalResponse.class);
-        String format = converter.getFormat();
-
+    public Answer getCapital(GetCapitalRequest getCapitalRequest) {
         PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
-        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry(),
-                "format", format));
-
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatModel.call(prompt);
 
-        return converter.convert(response.getResult().getOutput().getContent());
+        System.out.println(response.getResult().getOutput().getText());
+        String responseString;
+
+        try {
+            JsonNode jsonNode = objectMapper.readTree(response.getResult().getOutput().getText());
+            responseString = jsonNode.get("answer").asText();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Answer(responseString);
+        //return new Answer(response.getResult().getOutput().getText());
     }
+
     @Override
     public Answer getAnswer(Question question) {
         System.out.println("I was called");
